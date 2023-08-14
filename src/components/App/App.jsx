f1e3ebc3ea7f3d8023eeda4cf2b2e9c88a31b8ef
Rafael Yasis <rafaelyasis@gmail.com>
2023-08-14 17:01:14 -0500

    created components for header and tasks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,17 @@
+//IMPORTS
+import React from 'react';
+import Header from '../Header/Header';
+import TaskSection from '../TaskSection/TaskSection';
+import './App.css';
+
 import {useState} from 'react';
 
 function App () {
   
   return (
     <div className='App'>
-      <h1>TO DO:</h1>
+      <Header />
+      <TaskSection />
     </div>
   );
 
---
+++ src/components/TaskSection/TaskSection.jsx
@@ -0,0 +1,12 @@
+//IMPORTS
+import React, { useState, useEffect } from 'react';
+import './TaskSection.css';
+import axios from 'axios';
+
+function TaskSection() {
+    
+}
+
+
+
+export default TaskSection;
